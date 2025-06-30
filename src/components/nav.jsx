48b86beb15bb23f6640c9b4d62e6ea46ae54ba27
manish-gkv <manishkumarslmp@gmail.com>
2025-06-30 15:30:24 +0530

    added scroll progress in nav bar

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,21 +1,32 @@
+import { motion, useScroll } from 'motion/react'
+
 export default function Nav() {
+    const {scrollYProgress} = useScroll();
     return (
-        <nav className="sticky flex justify-between items-center py-4 px-8 border border-gray-200 rounded">
-            <div className="text-xl font-bold">Manish</div>
-            <div className="hidden md:flex gap-4 text-sm">
-                {/* Menu List */}
-                <div className="cursor-pointer">Home</div>
-                <div className="cursor-pointer">About</div>
-                <div className="cursor-pointer">Skills</div>
-                <div className="cursor-pointer">Projects</div>
-                <div className="cursor-pointer">Contact</div>
-            </div>
-            <div>
-                <button
-                    className="border border-gray-200 rounded px-3 py-1 text-sm font-medium"
-                >
-                    Resume
-                </button>
+        <nav className="sticky border border-gray-200 rounded">
+            <motion.div
+                className='bg-black w-full h-1 origin-left '
+                style={{
+                    scaleX:scrollYProgress
+                }}
+            ></motion.div>
+            <div className='flex justify-between items-center py-4 px-8'>
+                <div className="text-xl font-bold">Manish</div>
+                <div className="hidden md:flex gap-4 text-sm">
+                    {/* Menu List */}
+                    <div className="cursor-pointer">Home</div>
+                    <div className="cursor-pointer">About</div>
+                    <div className="cursor-pointer">Skills</div>
+                    <div className="cursor-pointer">Projects</div>
+                    <div className="cursor-pointer">Contact</div>
+                </div>
+                <div>
+                    <button
+                        className="border border-gray-200 rounded px-3 py-1 text-sm font-medium"
+                    >
+                        Resume
+                    </button>
+                </div>
             </div>
         </nav>
     );
