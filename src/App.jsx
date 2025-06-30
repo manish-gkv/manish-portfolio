cb100c5203ac221f9ef17aeb0a6e6b3149fa5d74
manish-gkv <manishkumarslmp@gmail.com>
2025-06-30 19:02:29 +0530

    adde skills section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Nav from "./components/nav";
 import Hero from "./components/hero";
 import Aboutme from "./components/aboutme";
-
+import Skills from "./components/skills";
 export default function App() {
   return (
     <div className="min-h-screen">
       <Nav />
       <Hero />
       <Aboutme />
+      <Skills />
     </div>
   );
 }
