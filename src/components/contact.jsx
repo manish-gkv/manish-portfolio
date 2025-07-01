d597f8f85e5a16b0a78ec98b70bff41101202dfa
manish-gkv <manishkumarslmp@gmail.com>
2025-07-01 13:49:56 +0530

    added site icon

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -17,7 +17,7 @@ export default function Contact() {
                             <input
                                 type="text"
                                 placeholder="Your Name"
-                                className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                                className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black"
                                 required
                             />
                         </div>
